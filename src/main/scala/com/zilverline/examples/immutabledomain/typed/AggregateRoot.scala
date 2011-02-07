553118e5798fb3cc4c09834c8f921aacb1d55e01
Erik Rozendaal <erik@deler.org>
2011-02-07 19:12:36 +0100

    Links to part 3 and 4 of blog. Minor improvements to typed AggregateRoot.scala.

--- src/main/scala/com/zilverline/examples/immutabledomain/typed/AggregateRoot.scala
+++ src/main/scala/com/zilverline/examples/immutabledomain/typed/AggregateRoot.scala
@@ -1,22 +1,22 @@
 package com.zilverline.examples.immutabledomain.typed
 
-trait EventSourced[ES <: EventSourced[ES, Event], Event] {
-  def applyEvent: Event => ES
+trait EventSourced[Event] {
+  def applyEvent: Event => EventSourced[Event]
 
   def unhandled(event: Event) = error("event " + event + " does not apply to " + this)
 }
 
-trait AggregateRoot[AR <: AggregateRoot[AR, Event], Event] extends EventSourced[AR, Event] {
+trait AggregateRoot[+AR <: AggregateRoot[AR, Event], Event] extends EventSourced[Event] {
   def uncommittedEvents: List[Event]
 
   def markCommitted: AR
 }
 
-trait AggregateFactory[AR <: AggregateRoot[AR, Event], Event] extends EventSourced[AR, Event] {
+trait AggregateFactory[AR <: AggregateRoot[AR, Event], Event] extends EventSourced[Event] {
   def loadFromHistory[T <: AR](history: Iterable[Event]): T = {
     var aggregate = applyEvent(history.head)
     for (event <- history.tail)
       aggregate = aggregate.applyEvent(event)
-    aggregate.markCommitted.asInstanceOf[T]
+    aggregate.asInstanceOf[AR].markCommitted.asInstanceOf[T]
   }
 }
